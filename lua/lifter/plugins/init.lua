dc568588e7536570c4792acb2e1ef1c0560641cd
Lifter <Sosite.my_dick@gmail.com>
2025-05-04 07:58:26 -0400

    Add new plugins

--- lua/lifter/plugins/init.lua
+++ lua/lifter/plugins/init.lua
@@ -2,7 +2,5 @@ return{
   "nvim-lua/plenary.nvim",
   "nvim-treesitter/nvim-treesitter",
   'neovim/nvim-lspconfig',
-  'jose-elias-alvarez/null-ls.nvim',
-  'MunifTanjim/prettier.nvim',
   "nvim-telescope/telescope.nvim"
 }
