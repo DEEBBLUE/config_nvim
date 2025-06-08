dfb54a89b79d203a11cfb427cadd75cf5b0c3bca
Lifter <Sosite.my_dick@gmail.com>
2025-06-08 14:07:04 -0400

    update config

--- lua/lifter/plugins/init.lua
+++ lua/lifter/plugins/init.lua
@@ -2,5 +2,6 @@ return{
   "nvim-lua/plenary.nvim",
   "nvim-treesitter/nvim-treesitter",
   'neovim/nvim-lspconfig',
+  'jose-elias-alvarez/null-ls.nvim',
   "nvim-telescope/telescope.nvim"
 }
--- lua/lifter/plugins/lsp/lsp_signature.lua
+++ lua/lifter/plugins/lsp/lsp_signature.lua
@@ -1,6 +1,6 @@
 return {
-  'ray-x/lsp_signature.nvim',
-  event = "VeryLazy",
+  "ray-x/lsp_signature.nvim",
+  event = "InsertEnter",
   config = function ()
     require('lsp_signature').setup({
       floating_window = false,
